fd8a4aa491b2c050818ae1581f11476550ad2cb6
Andrii Fylypiuk <fakeandrii@icloud.com>
2024-08-02 11:14:41 +0200

    Removed nullptr dereference

--- src/tracer.c
+++ src/tracer.c
@@ -1335,14 +1335,15 @@ static int __tracer_transition_tracing(
                                 new_bio_tracking_ptr;
                 }
 #else
-                LOG_DEBUG("!!! bd_ops is %p", (void*)bd_ops);
+                LOG_DEBUG("!!!? bd_ops is %p", (void*)bd_ops);
                 if(bd_ops){
-                        LOG_DEBUG("!!! bdev->bd_disk is %p", (void*)(bdev->bd_disk));
+                        LOG_DEBUG("!!!? bdev->bd_disk is %p", (void*)(bdev->bd_disk));
                         bdev->bd_disk->fops= bd_ops;
                 }
 #ifdef HAVE_BD_HAS_SUBMIT_BIO
-        LOG_DEBUG("!!! dev->sd_tracing_ops is %p", (void*)(dev->sd_tracing_ops));
-        bdev->bd_has_submit_bio=dev->sd_tracing_ops->has_submit_bio;
+                // LOG_DEBUG("!!!? dev is %p", (void*)(dev));
+                // LOG_DEBUG("!!!? dev->sd_tracing_ops is %p", (void*)(dev->sd_tracing_ops));
+                // bdev->bd_has_submit_bio=dev->sd_tracing_ops->has_submit_bio;
 #endif
 #endif
                 *dev_ptr = dev;
