01dfe34c6691e99ad05f51d36c1624c5111ff3a9
Andrii Fylypiuk <andrii.fylypiuk@datto.com>
2024-09-12 13:24:24 +0000

    Made functions inside cow_get_file_extents to return correct error codes, not shadowed ones

--- src/cow_manager.c
+++ src/cow_manager.c
@@ -1138,7 +1138,6 @@ int cow_get_file_extents(struct snap_device* dev, struct file* filp)
 
         ret = insert_vm_struct(task->mm, vma);
         if (ret < 0) {
-		ret = -EINVAL;
 		LOG_ERROR(ret, "insert_vm_struct() failed");
 		dattobd_vm_area_free(vma);
 		dattobd_mm_unlock(task->mm);
@@ -1147,7 +1146,6 @@ int cow_get_file_extents(struct snap_device* dev, struct file* filp)
 
         pg = alloc_pages(GFP_USER, get_order(cow_ext_buf_size));
 	if (!pg) {
-		ret = -ENOMEM;
 		LOG_ERROR(ret, "alloc_page() failed");
 		dattobd_vm_area_free(vma);
 		dattobd_mm_unlock(task->mm);
